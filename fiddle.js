9d03060be717f6a15e6996368d0835acefc2eed8
Goldensky09 <vikalp.parashar@gmail.com>
2016-10-14 13:08:08 +0530

    junk

--- fiddle.js
+++ fiddle.js
@@ -90,7 +90,7 @@
             	}
             };
         isSafari = function(){
-
+            return (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1);
         }
 
 		return {
