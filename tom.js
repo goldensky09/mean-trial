1b6e022fa2d9b10370d5e002c088408c20545fdf
Vikalp <vikalp.parashar@gmail.com>
2016-12-01 14:33:56 +0530

    Update tom.js

--- tom.js
+++ tom.js
@@ -1,62 +1,61 @@
-'use strict';
-
-// Define a component:
-var Button = React.createClass({
-  displayName: 'Button',
-
-  localHandleClick: function localHandleClick() {
-    this.props.localHandleClick(this.props.increment);
+var Card = React.createClass({
+  getInitialState: function(){
+    return {}
   },
-  render: function render() {
-    return React.createElement(
-      'button',
-      { onClick: this.localHandleClick },
-      '+',
-      this.props.increment
-    );
+  componentDidMount: function() {
+    var component = this;
+    $.get("https://api.github.com/users/" + this.props.login, function(data){
+      component.setState(data);
+    });
+  },
+  render: function(){
+    return (
+      <div>
+        <img src={this.state.avatar_url}  />
+        <h3>{this.state.name}</h3>
+        <hr />
+      </div>    
+    )
   }
 });
 
-var Result = React.createClass({
-  displayName: 'Result',
-
-  render: function render() {
-    return React.createElement(
-      'div',
-      null,
-      this.props.localCounter
+var Form = React.createClass({
+  handleSubmit: function(e){
+    e.preventDefault();
+    var loginInput = this.refs.login;
+    
+    //Add the card here
+    this.props.addCard(loginInput.value);
+    loginInput.value = "";
+  },
+  render: function() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input placeholder="github login" ref="login"/>
+        <button>Add</button>
+      </form>
     );
   }
 });
 
 var Main = React.createClass({
-  displayName: 'Main',
-
-  getInitialState: function getInitialState() {
-    return { counter: 0 };
+  getInitialState: function(){
+    return {logins: ['goldensky09']};
   },
-  handleClick: function handleClick(increment) {
-    this.setState({ counter: this.state.counter + increment });
+  addCard: function(loginToAdd){
+    this.setState({logins: this.state.logins.concat(loginToAdd)});
   },
-  render: function render() {
-    return React.createElement(
-      'div',
-      null,
-      React.createElement(Button, { localHandleClick: this.handleClick, increment: 1 }),
-      React.createElement(Button, { localHandleClick: this.handleClick, increment: 5 }),
-      React.createElement(Button, { localHandleClick: this.handleClick, increment: 10 }),
-      React.createElement(Button, { localHandleClick: this.handleClick, increment: 100 }),
-      React.createElement(Result, { localCounter: this.state.counter }),
-      React.createElement(
-        'h1',
-        null,
-        'Hello React!'
-      )
-    );
+  render: function(){
+    var cards = this.state.logins.map(function(login){
+      return (<Card login={login} />)
+    });
+    return (
+      <div>
+        <Form addCard={this.addCard} />
+        {cards}
+      </div>
+    )
   }
 });
 
-// Render a component to the browser:
-ReactDOM.render(React.createElement(Main, null), // What to render (an instance of the Main component)
-document.getElementById('container') // Where to render it
-);
+ReactDOM.render(<Main />, document.getElementById("container"));
