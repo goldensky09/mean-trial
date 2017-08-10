0bf660c476c75b6d3006b0b27b60dc08cad5f23b
Vikalp <vikalp.parashar@gmail.com>
2017-08-10 22:00:26 +0530

    Update spinner.js

--- spinner.js
+++ spinner.js
@@ -38,3 +38,17 @@ function Spinner (){
         return --count();
     }
 }
+
+input {
+    padding:10px 15px;
+    -webkit-border-radius: 25px;
+  -moz-border-radius: 25px;
+border-radius: 25px;
+    background: -moz-linear-gradient(top, #f6e6b4 0%, #ed9017 100%); /* firefox */
+    border: solid #ccc 3px;
+    box-shadow: 0 1px 2px #fff, 0 -1px 1px #666, inset 0 -1px 1px rgba(0,0,0,0.5), inset 0 1px 1px rgba(255,255,255,0.8);
+    -moz-box-shadow: 0 1px 2px #fff, 0 -1px 1px #666, inset 0 -1px 1px rgba(0,0,0,0.5), inset 0 1px 1px rgba(255,255,255,0.8);
+    -webkit-box-shadow: 0 1px 2px #fff, 0 -1px 1px #666, inset 0 -1px 1px rgba(0,0,0,0.5), inset 0 1px 1px rgba(255,255,255,0.8);
+    text-shadow: 0 1px 2px #fff;
+
+background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#f6e6b4), color-stop(100%,#ed9017)); /* webkit */font-size:20px}
