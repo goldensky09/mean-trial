927cedfc3a4daa65876846f399333448ccc25e52
Vikalp <vikalp.parashar@gmail.com>
2018-05-16 13:36:04 +0530

    key navigation

--- tom.js
+++ tom.js
@@ -1,3 +1,33 @@
+var li = $('li');
+var liSelected;
+$(window).keydown(function(e){
+    if(e.which === 40){
+        if(liSelected){
+            liSelected.removeClass('selected');
+            next = liSelected.next();
+            if(next.length > 0){
+                liSelected = next.addClass('selected');
+            }else{
+                liSelected = li.eq(0).addClass('selected');
+            }
+        }else{
+            liSelected = li.eq(0).addClass('selected');
+        }
+    }else if(e.which === 38){
+        if(liSelected){
+            liSelected.removeClass('selected');
+            next = liSelected.prev();
+            if(next.length > 0){
+                liSelected = next.addClass('selected');
+            }else{
+                liSelected = li.last().addClass('selected');
+            }
+        }else{
+            liSelected = li.last().addClass('selected');
+        }
+    }
+});
+
 /*
 Reference: http://jsfiddle.net/BB3JK/47/
 */
