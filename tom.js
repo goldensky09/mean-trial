0351c74e1ff8ebd9487b369097c55fdfeb3a99f9
Vikalp <vikalp.parashar@gmail.com>
2018-09-07 13:04:35 +0530

    Update tom.js

--- tom.js
+++ tom.js
@@ -26,3 +26,40 @@
     attributes: true
   });
 }();
+
+var targetNodes         = $(".myclass");
+var MutationObserver    = window.MutationObserver || window.WebKitMutationObserver;
+var myObserver          = new MutationObserver (mutationHandler);
+var obsConfig           = { childList: true, characterData: true, attributes: true, subtree: true };
+
+//--- Add a target node to the observer. Can only add one node at a time.
+targetNodes.each ( function () {
+    myObserver.observe (this, obsConfig);
+} );
+
+function mutationHandler (mutationRecords) {
+    console.info ("mutationHandler:");
+
+    mutationRecords.forEach ( function (mutation) {
+        console.log (mutation.type);
+        if (typeof mutation.removedNodes == "object") {
+            var jq = $(mutation.removedNodes);
+            console.log (jq);
+            console.log (jq.is("span.myclass2"));
+            console.log (jq.find("span") );
+        }
+    } );
+}
+
+/**************************************
+    jsFiddle only
+*/
+$("button").click ( function () {
+    var target   = $("#payload");
+    if (/censored/i.test (target.text () ) ) {
+        target.html ('<span class="myclass2">My <span class="boldly">vastly</span> improved</span> text.');
+    }
+    else {
+        target.html ('[censored!]');
+    }
+} );
