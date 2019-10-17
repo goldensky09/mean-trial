ebdeca038ccb4fc78765a65cc8dd42ad15aeee9a
Vikalp <vikalp.parashar@gmail.com>
2019-10-17 10:53:21 +0530

    d

--- tom.js
+++ tom.js
@@ -4,3 +4,43 @@ $([document.documentElement, document.body]).animate({
         scrollTop: $("#"+$(e.target).attr("href").split("#")[1]).offset().top
     }, 2000);
 })
+
+
+
+
+
+......................................................................................................................................... 
+Join online meeting 
+Trouble Joining? Try Skype Web App 
+
+Join by Phone 
+(INDONESIA) +62 21 3004 9395,, 82140106#
+(PHILIPPINES) Toll Free 1800 1110 2563,, 82140106#
+(INDIA) Toll Number Mumbai +91 22 6480 0004,, 82140106#
+(SINGAPORE) +65 6577 1162,, 82140106#
+(VIETNAM) Toll Free 1800 400 345 ,, 82140106#
+(JAPAN) Toll Free 012 077 5321,, 82140106#
+(INDIA) Toll Number Delhi +91 11 6480 0004,, 82140106#
+(NEW ZEALAND) Toll Free 0800 635 746,, 82140106#
+(AUSTRALIA) Toll Free 1800 319 416,, 82140106#
+(THAILAND) Toll Free 1800 012 795,, 82140106#
+(KOREA) Toll Free 0808 221 385,, 82140106#
+(INDIA) Toll Number Bangalore +91 80 6480 0004,, 82140106#
+(MALAYSIA) Toll Free 1800 383 501,, 82140106#
+(TAIWAN) Toll Free 0809 090 437,, 82140106#
+(HONG KONG) Toll Free 800 931 152,, 82140106#
+(CHINA) Toll Share 400 819 8774,, 82140106#
+(SRI LANKA) Toll Free 011 2029 049,, 82140106#
+Find a local number 
+
+Conference ID: 82140106 
+
+Forgot your dial-in PIN? | First online meeting? 
+
+- Join by SIP video conference system: Place a call to 411@vc.publicisgroupe.net and enter the Conference ID above.
+- Publicis Groupe employees can access www.groupevchelp.com for additional information on video conferencing services.
+Tip: For the best meeting experience always use a Skype certified headset. Avoid using your laptop mic and speakers to join Skype for business calls. 
+......................................................................................................................................... 
+
+
+
